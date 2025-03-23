b68d6d9dd1b451aa549234fbeb89767ca0a9e8d1
anandamith23 <anandkushwaha7892@gmail>
2025-03-24 01:43:14 +0530

    Calculatro pushes

---
+++ caklculator.py
@@ -0,0 +1,4 @@
+if age is < 18 not eligible fpr driving license
+    if age is > 18 and < 50  eligeble for driving license
+    if age is > 50 < = 90 risk for drivivng
+
--- hello.py
+++ hello.py
@@ -1,4 +1,16 @@
 a = int(input("enter first number:"))
 b = int(input("enter  second number:"))
-sum = a - b
+sum = a + b
 print(sum)
+
+x = float(input("enter first number:"))
+y = float(input("enter  second number:"))
+sub = x - y
+print(sub)
+
+
+mul = a * b
+print(mul)
+
+div = a/b
+print(div)
